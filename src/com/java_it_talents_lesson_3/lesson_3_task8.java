cd72c1db2c3a5fb8baafd6474a332da0328feea4
Marin Ivanov <marin.ivanov@imperiaonline.org>
2018-07-13 18:00:13 +0300

    Task 9 progress

--- src/com/java_it_talents_lesson_3/lesson_3_task8.java
+++ src/com/java_it_talents_lesson_3/lesson_3_task8.java
@@ -1,4 +1,25 @@
 package com.java_it_talents_lesson_3;
 
+import java.util.Scanner;
+
 public class lesson_3_task8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter 'n': ");
+        int inputN = sc.nextInt();
+        int step = inputN - 1;
+        if (inputN <= 0) {
+            System.out.print("Invalid input. The number must be a positive integer!");
+        } else {
+
+            for (int i = 1; i <= inputN; i++) {
+                for (int j = 0; j < inputN; j++) {
+                    System.out.print(step);
+                }
+                step += 2;
+                System.out.println();
+            }
+        }
+    }
 }
+
--- src/com/java_it_talents_lesson_3/lesson_3_task9.java
+++ src/com/java_it_talents_lesson_3/lesson_3_task9.java
@@ -1,4 +1,29 @@
 package com.java_it_talents_lesson_3;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.util.Scanner;
+
 public class lesson_3_task9 {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        1. Read two numbers
+        System.out.println("Please enter 'A': ");
+        int a = sc.nextInt();
+        System.out.println("Please enter 'A': ");
+        int b = sc.nextInt();
+        int sum = 0;
+        //        2. Print all numbers between A and B on step 2
+        for (int i = a; i <= b; i++) {
+            if ((i * i) % 3 == 0) {
+                System.out.print("Skip 3" + ", ");
+            }
+            System.out.print(i * i + ", ");
+        }
+    }
+
 }
+
+
